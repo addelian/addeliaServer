4d678df1436459588524920b36ab12feb44b8d1b
Nic Addelia <addelia.nicolas@gmail.com>
2020-06-20 22:08:24 -0500

    OAuth with Passport-Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/addeliaServer'
+    'mongoUrl' : 'mongodb://localhost:27017/addeliaServer',
+    'facebook': {
+        clientId: '283489389436811',
+        clientSecret: 'ecd159bf9af524951154a631f7d5d123'
+    }
 }
